9563582f24a5a45fdda44073025b7d182a1cdd11
Kavin Tjhan <ktjhan@Kavins-MBP.hsd1.ca.comcast.net>
2020-05-21 23:17:41 -0700

    heroku fix 5

--- server.js
+++ server.js
@@ -75,54 +75,21 @@ mongo.connect(process.env.DATABASE, (err, db) => {
           res.redirect('/profile');
     });
 
-    app.route("/").get((req, res) => {
-      //Change the response to render the Pug template
-      res.render(process.cwd() + '/views/pug/index', {title:'Hello', message: 'Please Login'});
+    app.route('/')
+      .get((req, res) => {
+        res.render(process.cwd() + '/views/pug/index');
     });
     
-    app.route("/login")
-      .post(passport.authenticate('local', {failureRedirect: '/'}),(req,res) => {
-        res.redirect('/profile');
-    });
-
-
     app.route('/profile')
     .get(ensureAuthenticated, (req, res) => {
          res.render(process.cwd() + '/views/pug/profile', {username: req.user.username});
     });
 
-    app.route('/register')
-      .post((req, res, next) => {
-          db.collection('users').findOne({ username: req.body.username }, function (err, user) {
-              if(err) {
-                  next(err);
-              } else if (user) {
-                  res.redirect('/');
-              } else {
-                  db.collection('users').insertOne(
-                    {username: req.body.username,
-                    password: req.body.password},
-                    (err, doc) => {
-                        if(err) {
-                            res.redirect('/');
-                        } else {
-                            next(null, user);
-                        }
-                    }
-                  )
-              }
-          })},
-        passport.authenticate('local', { failureRedirect: '/' }),
-        (req, res, next) => {
-            res.redirect('/profile');
-        }
-    );
-
     app.route('/logout')
       .get((req, res) => {
           req.logout();
           res.redirect('/');
-      });
+    });
 
     app.use((req, res, next) => {
       res.status(404)
